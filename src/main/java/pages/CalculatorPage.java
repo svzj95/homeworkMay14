26c26443d060b8fe028b6354721ee0ef7171967a
svzj95 <svzj95@gmail.com>
2018-05-17 18:16:53 +0300

    typePeriod function

--- src/main/java/pages/CalculatorPage.java
+++ src/main/java/pages/CalculatorPage.java
@@ -6,9 +6,20 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import steps.BaseSteps;
 
+import java.util.List;
+
 public class CalculatorPage extends BasePage {
     @FindBy(xpath = "//h2[contains(@class,'calculator-block__title')]")
     WebElement title;
+
+    @FindBy(xpath = "//span[@class='calculator__currency-field-text']")
+    List<WebElement> currencies;
+
+    @FindBy(xpath = "//input[@name='amount']")
+    WebElement inputDep;
+
+    @FindBy(xpath = "//select[@id='period']")
+    WebElement selectPeriod;
 
     CalculatorPage(){
         PageFactory.initElements(BaseSteps.getDriver(),this);
@@ -16,6 +27,24 @@ public class CalculatorPage extends BasePage {
 
     void checkTitle(String value){
         Assert.assertEquals(value, title.getText());
+    }
+
+    void chooseCurrency(String value){
+        for(WebElement i : currencies){
+            if(value.equals(i.getText())){
+                i.click();
+                break;
+            }
+        }
+
+    }
+
+    void typeSumOfDep(String value){
+        inputDep.sendKeys(value);
+    }
+
+    void typePeriod(String value){
+
     }
 
 }
